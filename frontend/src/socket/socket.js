3de6df441f94354d9af7bb806a8e912ad4e5940c
Ritikrajput-741 <ritik741rajput@gmail.com>
2026-08-02 17:31:47 +0530

    gggg

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:8081";
+const URL = "https://nova-chat-vmfa.onrender.com";
 
 export const socket = io(URL, {
   autoConnect: false,
