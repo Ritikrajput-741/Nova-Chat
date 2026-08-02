07a459d66ce6ff346a3020d5d546419399a8710d
RitikRajput <ritik741rajput@gmail.com>
2026-08-02 09:57:51 +0530

    redis file set

--- backend/redis/redis.js
+++ backend/redis/redis.js
@@ -1,5 +1,11 @@
 import Redis from "ioredis";
 
-export const redisClient = new Redis({
-  url: process.env.REDIS_URL,
+export const redisClient = new Redis(process.env.REDIS_URL);
+
+redisClient.on("connect", () => {
+  console.log("✅ Redis Connected");
+});
+
+redisClient.on("error", (err) => {
+  console.log("❌ Redis Error:", err.message);
 });
